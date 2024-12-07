cac2a08d47a225d6367858f6a03c41896ac5debc
William Oktavianus <williamoktavianus@icloud.com>
2024-12-07 10:29:19 +0700

    Refactoring Navbar

--- app/components/navbar/menu-icon.js
+++ app/components/navbar/menu-icon.js
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import Image from "next/image";
 
 export default function MenuIcon({ isMenuOpen, toggleMenu }) {
   return (
@@ -8,11 +8,15 @@ export default function MenuIcon({ isMenuOpen, toggleMenu }) {
       className="lg:hidden text-2xl p-2 z-50"
       aria-label="Toggle menu"
     >
-      {isMenuOpen ? (
-        <img src="/icons/close.svg" alt="Close Icon" className="w-8 h-8" />
-      ) : (
-        <img src="/icons/menu.svg" alt="Menu Icon" className="w-8 h-8" />
-      )}
+      <div className="relative w-8 h-8">
+        {isMenuOpen ? (
+          // Close icon.
+          <Image src="/icons/close.svg" alt="Close Icon" layout="fill" />
+        ) : (
+          // Open icon.
+          <Image src="/icons/menu.svg" alt="Menu Icon" layout="fill" />
+        )}
+      </div>
     </button>
   );
 }
