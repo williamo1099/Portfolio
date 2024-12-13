f942b631bc9baec6f498e6a44c8df19c93b5a2bd
William Oktavianus <williamoktavianus@icloud.com>
2024-12-13 21:28:52 +0700

    Fixing minor view bug in About Me

--- app/about-me/components/trait-list.js
+++ app/about-me/components/trait-list.js
@@ -4,7 +4,7 @@ import { traits } from "@/data/traits";
 
 export default function TraitList() {
   return (
-    <div className="flex flex-row space-x-4 items-center justify-center w-full mb-20">
+    <div className="flex flex-row space-x-4 items-center justify-center w-full mb-20 lg:mb-0">
       {traits.map((trait, index) => (
         <TraitCircle key={index} trait={trait} />
       ))}
