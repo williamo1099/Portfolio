ed2b804adf98f66c8af8006ff6ffd3cdb47b2300
William Oktavianus <williamoktavianus@icloud.com>
2025-01-17 16:09:59 +0700

    Updating projects

--- data/projects.js
+++ data/projects.js
@@ -1,6 +1,3 @@
-import HighlightedText from "@/app/components/highlighted-text";
-import Link from "next/link";
-
 export const personalProjects = [
   {
     title: "TaskBoard",
@@ -11,28 +8,28 @@ export const personalProjects = [
 
 export const professionalProjects = [
   {
-    title: "GLUP",
-    image: "/images/projects/professional/ewm/poster.png",
-    stacks: ["nextjs", "laravel", "mysql"],
+    title: "GLUP POS Mobile App + Admin Sales Portal",
+    image: "/images/projects/professional/glup/poster.png",
+    stacks: ["nextjs", "laravel", "vite", "tailwind", "mysql"],
   },
   {
     title: "Era Warna Mandiri BI Mobile App",
     image: "/images/projects/professional/ewm/poster.png",
-    stacks: ["flutter"],
+    stacks: ["flutter", "codeigniter"],
   },
   {
     title: "Matric Seal ERP System",
     image: "/images/projects/professional/matric/poster.png",
-    stacks: ["laravel", "mysql"],
+    stacks: ["laravel", "vite", "tailwind", "mysql"],
   },
   {
     title: "UNICO Tractors ERP System",
-    image: "/images/projects/professional/matric/poster.png",
+    image: "/images/projects/professional/unico/poster.png",
     stacks: ["codeigniter", "mysql"],
   },
   {
     title: "Project KIRI",
     image: "/images/projects/professional/projectkiri/poster.png",
-    stacks: [],
+    stacks: ["codeigniter", "mapbox"],
   },
 ];
