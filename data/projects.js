47c8898f269404706d42e54c621cbdb36608976a
William Oktavianus <williamoktavianus@icloud.com>
2025-02-06 20:22:13 +0700

    Hiding professional projects temporarily

--- data/projects.js
+++ data/projects.js
@@ -6,30 +6,31 @@ export const personalProjects = [
   },
 ];
 
-export const professionalProjects = [
-  {
-    title: "GLUP POS Mobile App + Admin Sales Portal",
-    image: "/images/projects/professional/glup/poster.png",
-    stacks: ["nextjs", "laravel", "vite", "tailwind", "mysql"],
-  },
-  {
-    title: "Era Warna Mandiri BI Mobile App",
-    image: "/images/projects/professional/ewm/poster.png",
-    stacks: ["flutter", "codeigniter"],
-  },
-  {
-    title: "Matric Seal ERP System",
-    image: "/images/projects/professional/matric/poster.png",
-    stacks: ["laravel", "vite", "tailwind", "mysql"],
-  },
-  {
-    title: "UNICO Tractors ERP System",
-    image: "/images/projects/professional/unico/poster.png",
-    stacks: ["codeigniter", "mysql"],
-  },
-  {
-    title: "Project KIRI",
-    image: "/images/projects/professional/projectkiri/poster.png",
-    stacks: ["codeigniter", "mapbox"],
-  },
-];
+export const professionalProjects = [];
+// export const professionalProjects = [
+//   {
+//     title: "GLUP POS Mobile App + Admin Sales Portal",
+//     image: "/images/projects/professional/glup/poster.png",
+//     stacks: ["nextjs", "laravel", "vite", "tailwind", "mysql"],
+//   },
+//   {
+//     title: "Era Warna Mandiri BI Mobile App",
+//     image: "/images/projects/professional/ewm/poster.png",
+//     stacks: ["flutter", "codeigniter"],
+//   },
+//   {
+//     title: "Matric Seal ERP System",
+//     image: "/images/projects/professional/matric/poster.png",
+//     stacks: ["laravel", "vite", "tailwind", "mysql"],
+//   },
+//   {
+//     title: "UNICO Tractors ERP System",
+//     image: "/images/projects/professional/unico/poster.png",
+//     stacks: ["codeigniter", "mysql"],
+//   },
+//   {
+//     title: "Project KIRI",
+//     image: "/images/projects/professional/projectkiri/poster.png",
+//     stacks: ["codeigniter", "mapbox"],
+//   },
+// ];
