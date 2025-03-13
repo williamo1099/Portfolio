bc6b2a091c8c052e5e481c39df5aef4e46644a8e
William Oktavianus <williamoktavianus@icloud.com>
2025-03-13 18:16:04 +0700

    Removing redirect from config

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,6 @@
 const nextConfig = {
   // Enable static export.
   output: "export",
-
-  async redirects() {
-    return [
-      {
-        source: "/home",
-        destination: "/",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
