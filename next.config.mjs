b1eebd275a139e20616130e8237e538b9aa8f71d
William Oktavianus <williamoktavianus@icloud.com>
2025-03-13 19:17:15 +0700

    Disabling image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   // Enable static export.
   output: "export",
+
+  // Disable image optimization.
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
